4736da937c3608fa09dc1d3ad1210f6b49b4b49f
Your Name <you@example.com>
2022-01-05 23:00:51 -0500

    Actualizacion de tildes

--- BOT.py
+++ BOT.py
@@ -1,16 +1,18 @@
 import pyautogui
 import time
-import unicodedata
+import keyboard
 
 Spam = open("spam.txt","r", encoding='utf-8')
 time.sleep(5) 
 
 with open('spam.txt', encoding='utf-8') as f: 
     for copiar in f:
-        print(copiar)
+        keyboard.write(copiar, delay=0.5)
         pyautogui.typewrite(copiar)
         pyautogui.press("enter")
         time.sleep(5)
+
+
 
 
 
